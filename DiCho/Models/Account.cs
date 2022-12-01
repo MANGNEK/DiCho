b0dc458f69aaaafcd9a11b01833e2dafa8b917f6
MANGNEK <trungnguyen.kc5@gmail.com>
2022-12-01 18:56:20 +0700

    first commit

---
+++ DiCho/Models/Account.cs
@@ -0,0 +1,38 @@
+namespace DiCho.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Account
+    {
+        public int AccountID { get; set; }
+
+        [StringLength(12)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [StringLength(10)]
+        public string Salt { get; set; }
+
+        public bool Active { get; set; }
+
+        [StringLength(150)]
+        public string FullName { get; set; }
+
+        public int? RoleID { get; set; }
+
+        public DateTime? LastLogin { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+}
